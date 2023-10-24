b841c4f3f89138e58fbba4a01b368db1abb0d83c
Nazar <nz.shevchuk.2002@gmail.com>
2023-10-24 13:15:10 +0300

    Output posts

--- wp-content/themes/MyTheme/index.php
+++ wp-content/themes/MyTheme/index.php
@@ -1,3 +1,27 @@
-<?php get_header()?>
+<?php get_header() ?>
 <h1>Hello, world!</h1>
-<?php get_footer()?>
+
+<div class="container">
+  <div class="row">
+    <?php if (have_posts()) : ?>
+      <?php while (have_posts()) : the_post(); ?>
+        <div class="col-md-12">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">
+                <a href="<?php the_permalink()?>"><?php the_title()?></a>
+              </h5>
+              <p class="card-text"><?php the_content('<button>Next</button>')?></p>
+              <a href="<?php the_permalink()?>" class="btn btn-primary">Go to post</a>
+            </div>
+          </div>
+        </div>
+      <?php endwhile; ?>
+    <?php else : ?>
+      <p>Don't have posts</p>
+    <?php endif; ?>
+  </div>
+</div>
+
+<?php get_footer() ?>
