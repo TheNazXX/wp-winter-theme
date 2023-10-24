f5c7a5d1d5cab9c799aff25cc988819d6269b5f4
Nazar <nz.shevchuk.2002@gmail.com>
2023-10-24 11:47:29 +0300

    wp_basic

---
+++ wp-content/themes/MyTheme/functions.php
@@ -0,0 +1,16 @@
+<?php 
+  /*
+   Styles and Scripts
+  */
+
+  function wp1_scripts(){
+    wp_enqueue_style('wp1-bootstrap-css', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css');
+    wp_enqueue_style('wp1-style', get_stylesheet_uri());
+
+    wp_enqueue_script('wp1-popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js');
+    wp_enqueue_script('wp1-bootstrap-js', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js');
+  };
+
+  add_action('wp_enqueue_scripts', 'wp1_scripts');
+?>
+
