7270380011a3d5cdf3a05f02cff3be659944fb9c
Nazar <nz.shevchuk.2002@gmail.com>
2023-10-24 12:04:02 +0300

    styles & scripts hooks

--- wp-content/themes/MyTheme/functions.php
+++ wp-content/themes/MyTheme/functions.php
@@ -7,8 +7,8 @@
     wp_enqueue_style('wp1-bootstrap-css', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css');
     wp_enqueue_style('wp1-style', get_stylesheet_uri());
 
-    wp_enqueue_script('wp1-popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js');
-    wp_enqueue_script('wp1-bootstrap-js', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js');
+    wp_enqueue_script('wp1-popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js', ['jquery'], false, true);
+    wp_enqueue_script('wp1-bootstrap-js', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js', ['jquery'], false, true);
   };
 
   add_action('wp_enqueue_scripts', 'wp1_scripts');
